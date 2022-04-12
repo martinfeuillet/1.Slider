4b7067ddf137fb0d1da0a26ee6f50f7e510a3ffe
martinfeuillet <martin.feuillet92@gmail.com>
2022-04-12 14:25:33 +0200

    first commit

---
+++ src/Components/Slider/BtnSlider.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import leftArrow from './icons/left-arrow.svg'
+import rightArrow from './icons/right-arrow.svg'
+
+export default function BtnSlider({moveSlide, direction}) {
+  return (
+    <button 
+    className={direction ==="next" ? "btn-slide next" : "btn-slide prev"}
+    onClick={moveSlide}
+    >
+        <img src={direction === "next" ? rightArrow : leftArrow} alt="" />
+    </button>
+  )
+}
